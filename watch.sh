86fcda81cc63d0a952e5f4703898c601d16c5b6b
pascalwhoop <accounts@pascalbrokmeier.de>
2018-07-08 15:44:47 +0200

    amend for build update

--- watch.sh
+++ watch.sh
@@ -3,16 +3,11 @@ echo "Watching file tree for changed files. Running tests when it occurs"
 pkill pdflatex
 pkill latexmk
 ./clean_tex.sh
-makepdf() {
-    cd src/
-    ./compile.sh
-    cd ../
-}
 
 #zathura src/main.pdf &
 
 inotifywait -rm --exclude "aux|bib~|tex~|toc|log|bbl|blg|fdb_latexmk|fls|glo|ist|out|pdf|swp|git\/" -e modify ./src | while read change; do
-    makepdf 
+    ./compile.sh
     echo "change detected"
     echo $change
 done
